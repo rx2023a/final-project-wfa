9e6b0f3b4a94ba895ba09c7e98b181c2deb96e40
Wahyu Tri Saputro <91375670+KobamYo@users.noreply.github.com>
2023-11-21 13:46:22 +0700

    Can slice but still at random occurence

---
+++ Assets/Scripts/SliceObject.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;
+using UnityEngine.InputSystem;
+using Unity.VisualScripting;
+
+public class SliceObject : MonoBehaviour
+{
+    public Transform startSlicePoint;
+    public Transform endSlicePoint;
+    public VelocityEstimator velocityEstimator;
+    public LayerMask sliceableLayer;
+    public Material crossSectionMaterial;
+
+    public float cutForce = 2000.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bool hasHit = Physics.Linecast(startSlicePoint.position, endSlicePoint.position, out RaycastHit hit, sliceableLayer);
+        if(hasHit)
+        {
+            GameObject target = hit.transform.gameObject;
+            Slice(target);
+        }
+    }
+
+    public void Slice(GameObject target)
+    {
+        Vector3 velocity = velocityEstimator.GetVelocityEstimate();
+        Vector3 planeNormal = Vector3.Cross(endSlicePoint.position - startSlicePoint.position, velocity);
+        planeNormal.Normalize();
+
+        SlicedHull hull = target.Slice(endSlicePoint.position, Vector3.up);
+
+        if(hull != null) 
+        {
+            GameObject upperHull = hull.CreateUpperHull(target, crossSectionMaterial);
+            SetupSlicedComponent(upperHull);
+
+            GameObject lowerHull = hull.CreateLowerHull(target, crossSectionMaterial);
+            SetupSlicedComponent(lowerHull);
+
+            Destroy(target);
+        }
+    }
+
+    public void SetupSlicedComponent(GameObject slicedObject)
+    {
+        Rigidbody rigidbody = slicedObject.AddComponent<Rigidbody>();
+        MeshCollider collider = rigidbody.AddComponent<MeshCollider>();
+        collider.convex = true;
+        rigidbody.AddExplosionForce(cutForce, slicedObject.transform.position, 1);
+    }
+}
