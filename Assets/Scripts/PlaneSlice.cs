ee70860f2370bd6d2867b40ea632d55c4e5739f7
Wahyu Tri Saputro <91375670+KobamYo@users.noreply.github.com>
2023-12-14 14:58:59 +0700

    WIP Undo + UI

--- Assets/Scripts/PlaneSlice.cs
+++ Assets/Scripts/PlaneSlice.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using EzySlice;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlaneSlice : MonoBehaviour
 {
-    // public Transform planeDebug;
     public Transform firstPlane;
     public Transform secondPlane;
     public GameObject target;
     public Material crossSectionMaterial;
+
+    public AudioClip crackingSound;
+    private AudioSource audioSource;
+
+    private Stack<GameObject> previousSlices = new Stack<GameObject>(); // Stack to store previous slices
+    private GameObject clonedObject; // Reference to the cloned object
+    private bool isClonedHidden = true; // Flag to track cloned object visibility
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            // Add AudioSource component if not already present
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+        audioSource.clip = crackingSound;
+    }
 
     // Update is called once per frame
     void Update()
@@ -18,6 +36,12 @@ public class PlaneSlice : MonoBehaviour
         if(Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             Slice(target);
+            PlayCrackingSound(); // Play the cracking sound
+        }
+
+        if (Keyboard.current.zKey.wasPressedThisFrame)
+        {
+            UndoSlice();
         }
     }
 
@@ -40,6 +64,62 @@ public class PlaneSlice : MonoBehaviour
                 Destroy(lowerHull);
             }
             Destroy(target);
+
+            // Store the sliced object state into the stack
+            GameObject slicedObject = Instantiate(target);
+            previousSlices.Push(slicedObject);
+
+            // Hide the cloned object
+            clonedObject = slicedObject;
+            SetObjectVisibility(clonedObject, false);
+            isClonedHidden = true;
         }
     }
+
+    void PlayCrackingSound()
+    {
+        if (crackingSound != null && audioSource != null)
+        {
+            audioSource.Play();
+        }
+    }
+
+    void UndoSlice()
+    {
+        if (previousSlices.Count > 0)
+        {
+            DestroyHulls(); // Destroy the created hulls
+
+            // Restore the previous state by popping from the stack and setting it as the new target
+            GameObject previousState = previousSlices.Pop();
+            Destroy(target); // Destroy the current sliced object
+            target = previousState;
+
+            // Show the cloned object
+            SetObjectVisibility(clonedObject, true);
+            isClonedHidden = false;
+        }
+    }
+    void DestroyHulls()
+    {
+        // Your logic to destroy the hulls created after slicing
+        // For example:
+        // Destroy(upperHull);
+        // Destroy(lowerHull);
+        // Destroy(middleHull);
+        // Destroy(finalLowerHull);
+    }
+
+    void SetObjectVisibility(GameObject obj, bool isVisible)
+    {
+        if (obj != null)
+        {
+            Renderer[] renderers = obj.GetComponentsInChildren<Renderer>();
+            foreach (Renderer rend in renderers)
+            {
+                rend.enabled = isVisible;
+            }
+        }
+    }
+
 }
