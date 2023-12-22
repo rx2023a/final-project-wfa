cc9c9d97cebce3e92e79041ba5af9ec84808cfa4
Wahyu Tri Saputro <91375670+KobamYo@users.noreply.github.com>
2023-12-22 16:05:35 +0700

    MENYERAH :(

--- Assets/Scripts/PassthroughManager.cs
+++ Assets/Scripts/PassthroughManager.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class PassthroughManager : MonoBehaviour
 {
-    public OVRPassthroughLayer passtrough;
+    public OVRPassthroughLayer passthrough;
     public OVRInput.Button button;
     public OVRInput.Touch touch;
     public OVRInput.Controller controller;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        if (OVRManager.IsPassthroughRecommended())
+        {
+            passthrough.enabled = true;
+
+            // Set camera background to transparent
+            OVRCameraRig ovrCameraRig = GameObject.Find("OVRCameraRig").GetComponent<OVRCameraRig>();
+            var centerCamera = ovrCameraRig.centerEyeAnchor.GetComponent<Camera>();
+            centerCamera.clearFlags = CameraClearFlags.SolidColor;
+            centerCamera.backgroundColor = Color.clear;
+
+            // Ensure your VR background elements are disabled
+        }
+        else
+        {
+            passthrough.enabled = false;
+
+            // Ensure your VR background elements are enabled
+        }
+    }
     void Update()
     {
         if(OVRInput.GetDown(button, controller)) 
         {
-            passtrough.hidden = !passtrough.hidden;
+            passthrough.hidden = !passthrough.hidden;
         }
     }
 }
